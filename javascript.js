c8d58aaf9edac6c4126f463ed993fa2c5dbc9745
vespernia <88417181+vespernia@users.noreply.github.com>
2021-09-09 16:12:13 +0900

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,9 @@
+'use strict';
+
+$(document).ready(function() {
+  $('#open_nav').on('click', function() {
+    $('#except_hamburger', '#hamburger').toggleClass('show');
+  });
+});
+
+console.log('hello');
